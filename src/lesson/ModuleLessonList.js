acc8fe9cb0edad714b4eac24caadc7e0ce5c2326
h4rdw1r3 <vinaugust6@gmail.com>
2020-06-05 02:14:46 +0530

    add link to lesson content page
    add lesson form modal template

--- src/lesson/ModuleLessonList.js
+++ src/lesson/ModuleLessonList.js
@@ -8,26 +8,26 @@ export default function ModuleLessonList() {
     <>
       <PageHeader title="Integers" />
       <Card style={{ margin: '8px 32px' }}
-        title="Addition of integers"
+        title={<Link to={`${location.pathname}/1/contents`}>Addition of integers</Link>}
       >
         <List>
-          <Link to={`${location.pathname}/content-id`}>
+          <Link to={`${location.pathname}/1/contents/22`}>
             <List.Item >Adding number with different signs</List.Item>
           </Link>
-          <Link to={`${location.pathname}/content-id`} >
+          <Link to={`${location.pathname}/2/contents/12`} >
             <List.Item>Subraction number with same signs</List.Item>
           </Link>
         </List>
       </Card>
 
       <Card style={{ margin: '8px 32px' }}
-        title="Multiplicaiton of integers"
+        title={<Link to={`${location.pathname}/1/contents`}>Multiplication of integers</Link>}
       >
         <List>
-          <Link to={`${location.pathname}/content-id`} >
+          <Link to={`${location.pathname}/1/contents/2`} >
             <List.Item>Multiplying positive and negative numbers</List.Item>
           </Link>
-          <Link to={`${location.pathname}/content-id`} >
+          <Link to={`${location.pathname}/1/contents/3`} >
             <List.Item>Why negative times a negative is positive</List.Item>
           </Link>
         </List>
