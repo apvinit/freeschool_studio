86007a9e2c5e50495114e9f09c29aa21a8f5c3b7
h4rdw1r3 <vinaugust6@gmail.com>
2020-06-04 17:46:51 +0530

    add button for deleting categories

--- src/category/CategoryList.js
+++ src/category/CategoryList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Card, PageHeader, Avatar, Empty, Spin } from 'antd'
+import { Card, PageHeader, Avatar, Empty, Spin, Space, Button } from 'antd'
 import AddCategory from './AddCategory'
 import Axios from 'axios'
 
@@ -9,12 +9,16 @@ export default function CategoryList() {
   const [items, setItems] = useState([])
 
   useEffect(() => {
+    _fetchData()
+  }, [])
+
+  function _fetchData() {
     Axios.get('https://api.freeschool.org.in/api/categories')
       .then(resp => {
         setItems(resp.data)
         setLoading(false)
       })
-  }, [])
+  }
 
   return (
     loading ? <div
@@ -28,9 +32,19 @@ export default function CategoryList() {
             items.map(i => <React.Fragment key={i.id}>
               <Card style={{ margin: '8px 32px' }} >
                 <Avatar size="large" />
-                <h3 style={{ display: 'inline', marginLeft: '16px' }}>
-                  {i.title}
-                </h3>
+                <Space>
+                  <h3 style={{ display: 'inline', marginLeft: '16px' }}>
+                    {i.title}
+                  </h3>
+
+                  <Button danger
+                    size="small"
+                    onClick={(_) => {
+                      Axios.delete(`https://api.freeschool.org.in/api/categories/${i.id}`)
+                        .then(_ => _fetchData())
+                    }}>Delete</Button>
+
+                </Space>
               </Card>
             </React.Fragment>
             )
