e5f494fef5d9a8ca810b0ebe4562cf3c3986b680
h4rdw1r3 <vinaugust6@gmail.com>
2020-06-07 17:00:21 +0530

    get contents for the lesson

--- src/content/ContentPage.js
+++ src/content/ContentPage.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { PageHeader, Card } from 'antd'
+import { useLocation } from 'react-router-dom'
 
 export default function ContentPage() {
+
+  let location = useLocation()
+
   return (
     <>
-      <PageHeader title="Adding number with different signs" />
+      <PageHeader title={location.state.data.title} />
       <Card>
-        <p>Some content of the lesson (it may be video or rich text)</p>
+        {/* <p>Some content of the lesson (it may be video or rich text)</p> */}
       </Card>
     </>
   )
