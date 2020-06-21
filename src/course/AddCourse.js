3c51d9b15367a3a1b25172712220f0a9d0583b1c
h4rdw1r3 <vinaugust6@gmail.com>
2020-06-22 00:27:15 +0530

    add rest of the fields to the add course form

--- src/course/AddCourse.js
+++ src/course/AddCourse.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
-import { Button, Input, Modal } from 'antd'
+import { Button, Input, Modal, Upload } from 'antd'
 import Form from 'antd/lib/form/Form'
-import { addCourse } from '../service/remote'
+import { addCourse, uploadMedia } from '../service/remote'
+import { UploadOutlined } from '@ant-design/icons'
 
 export default function AddCourse(props) {
   const [showModal, setShowModal] = useState(false)
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
+  const [cover, setCover] = useState('')
+  const [lang, setLang] = useState('')
+  const [createdBy, setCreatedBy] = useState('')
 
   let onCancel = () => {
     setShowModal(false)
@@ -20,7 +24,12 @@ export default function AddCourse(props) {
     }
     const data = {
       title,
-      category_id: props.categoryID
+      description,
+      cover,
+      lang,
+      created_by: createdBy,
+      category_id: props.categoryID,
+      draft: true
     }
     await addCourse(data)
     resetState()
@@ -38,6 +47,8 @@ export default function AddCourse(props) {
   function resetState() {
     setTitle('')
     setDescription('')
+    setCover('')
+    setLang('')
   }
 
   return (
@@ -48,13 +59,35 @@ export default function AddCourse(props) {
       <Modal title="Add Course"
         visible={showModal}
         onOk={onOk}
-        onCancel={onCancel}>
+        onCancel={onCancel}
+        destroyOnClose={true}
+      >
         <Form>
+          <Upload
+            accept="image/*"
+            customRequest={async (obj) => {
+              let resp = await uploadMedia(obj.file)
+              setCover(resp.data.id)
+              obj.onSuccess(resp, obj.file)
+            }}
+          >
+            <Button>
+              <UploadOutlined />
+              Upload
+            </Button>
+          </Upload>
+          <div style={{ marginBottom: '8px' }}></div>
           <Input placeholder="Title" size="large"
             value={title} onChange={e => setTitle(e.target.value)} />
           <div style={{ marginBottom: '8px' }}></div>
           <Input placeholder="Description" size="large"
             value={description} onChange={e => setDescription(e.target.value)} />
+          <div style={{ marginBottom: '8px' }}></div>
+          <Input placeholder="Language" size="large"
+            value={lang} onChange={e => setLang(e.target.value)} />
+          <div style={{ marginBottom: '8px' }}></div>
+          <Input placeholder="Author" size="large"
+            value={createdBy} onChange={e => setCreatedBy(e.target.value)} />
         </Form>
       </Modal>
 
